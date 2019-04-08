7c718a74be9cbe863ecbc5d8aaff1cf00f10d384
github username <github email address>
2019-04-08 16:03:50 -0500

    Set up basic ProjectController methods

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,42 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects =  Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update
+    end
+  end
+
+  def destroy
+    @project.destroy
+  end
+
+  private
+    def find_project
+      @project = Project.find(params[:id])
+    end
+
+    def project_params
+      params.require(:project).permit(:name, :status, :pattern_info)
+    end
+
 end
