ad88dd2bf490cf94504e7f7e2d7eaa7754e0aa23
github username <github email address>
2019-04-08 12:59:57 -0500

    Add user migration & devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Routes for Google authentication
   get 'auth/:provider/callback', to: 'sessions#googleAuth'
-  get 'auth/failure', to: redirect(‘/’)
+  get 'auth/failure', to: redirect('/')
   # Routes for Pinterest authentication
   # '/auth/pinterest'
 
