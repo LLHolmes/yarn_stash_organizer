a02c1d736e7a0b5adf0f3195c811093b58592c2b
github username <github email address>
2019-04-09 15:30:25 -0500

    Refactor Brand _form

--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -1,2 +1,11 @@
 module BrandsHelper
+
+  def material_choices
+    [ 'Acrylic', 'Cotton', 'Wool', 'Alpaca', 'Other' ]
+  end
+
+  def weight_choices
+    [ '0 - Lace', '1 - Super Fine', '2 - Fine', '3 - Light', '4 - Medium', '5 - Bulky', '6 - Super Bulky', '7 - Jumbo', 'Novelty' ]
+  end
+
 end
