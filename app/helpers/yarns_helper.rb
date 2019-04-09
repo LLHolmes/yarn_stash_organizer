435f48918581fa0f5cf4620e589445227df73510
github username <github email address>
2019-04-09 15:50:34 -0500

    Refactor Brand show colors of yarn again

--- app/helpers/yarns_helper.rb
+++ app/helpers/yarns_helper.rb
@@ -1,20 +1,18 @@
 module YarnsHelper
 
   def yarn_amount(yarn)
-    if yarn.count && yarn.scrap
-      if yarn.scrap.to_f == 0.0 && yarn.count == 1
-        "(#{yarn.count} skein)"
-      elsif yarn.scrap.to_f == 0.0 && yarn.count > 1
-        "(#{yarn.count} skeins)"
-      elsif yarn.count == 1
+    if yarn.count && yarn.count == 1
+      if yarn.scrap && yarn.scrap.to_f > 0.0
         "(#{yarn.count} skein, #{yarn.scrap.to_f} oz. scrap)"
-      elsif yarn.count > 1
-        "(#{yarn.count} skeins, #{yarn.scrap.to_f} oz. scrap)"
+      else
+        "(#{yarn.count} skein)"
       end
-    elsif yarn.count && yarn.count == 1
-      "(#{yarn.count} skein)"
     elsif yarn.count && yarn.count > 1
-      "(#{yarn.count} skeins)"
+      if yarn.scrap && yarn.scrap.to_f > 0.0
+        "(#{yarn.count} skeins, #{yarn.scrap.to_f} oz. scrap)"
+      else
+        "(#{yarn.count} skeins)"
+      end
     elsif yarn.scrap && yarn.scrap.to_f > 1
       "(#{yarn.scrap.to_f} oz. scrap)"
     end
