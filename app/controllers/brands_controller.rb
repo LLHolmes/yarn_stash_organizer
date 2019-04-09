3913b7ce7c47ca4aaab8f4a3560aefff9eb456df
github username <github email address>
2019-04-08 19:29:58 -0500

    Set up basic BrandController methods

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,38 @@
 class BrandsController < ApplicationController
+  before_action :find_brand, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(brand_params)
+    if @brand.save
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @brand.update
+    end
+  end
+
+  def destroy
+    @brand.destroy
+  end
+
+  private
+    def find_brand
+      @brand = Brand.find(params[:id])
+    end
+
+    def brand_params
+      params.require(:brand).permit(:name, :material, :weight, :hook, :needle, :skein_weight, :skein_length)
+    end
+
 end
