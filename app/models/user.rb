096fc3f970a1c67d65f6a302da406a3b01ba700c
github username <github email address>
2019-04-09 13:47:18 -0500

    Add User methods for refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,40 @@ class User < ApplicationRecord
 
   validates_uniqueness_of :email
 
+  # def projects_finished
+  #   self.projects.select { |project| project.status == "Finished" }
+  # end
+  #
+  # def projects_wip
+  #   self.projects.select { |project| project.status == "In Progress" }
+  # end
+  #
+  # def projects_upcoming
+  #   self.projects.select { |project| project.status == "Upcoming" }
+  # end
+  #
+  # def stash
+  #   self.projects.detect { |project| project.name == "Stash" }
+  # end
+  #
+  # def yarns_by_brand
+  #   self.yarns.sort_by { |yarn| yarn.color }.sort_by { |yarn| yarn.brand.name }
+  # end
+  #
+  # def yarns_sorted
+  #   self.yarns_by_brand.sort_by { |yarn| yarn.brand.material }.sort_by { |yarn| yarn.brand.weight }
+  # end
+  #
+  # def brands_sorted
+  #   self.brands.sort_by { |brand| brand.name }.sort_by { |brand| brand.material }
+  # end
+  #
+  # def brand_by_weight(wt)
+  #   self.brands_sorted.select { |brand| brand.weight == wt }
+  # end
+  #
+  # def brand_yarns(given_brand)
+  #   self.yarns.select { |yarn| yarn.brand == given_brand }
+  # end
 
 end
