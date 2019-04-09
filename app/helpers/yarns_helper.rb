8102654352cf0bb0787bbdb0eac136310e8508f4
github username <github email address>
2019-04-09 15:42:47 -0500

    Refactor Brand show colors of yarn

--- app/helpers/yarns_helper.rb
+++ app/helpers/yarns_helper.rb
@@ -1,25 +1,23 @@
 module YarnsHelper
 
-  # def alphabetical_yarn_with_amount(brand)
-  #   <% brand.yarns_by_color.each do |yarn| %>
-  #     <% if yarn.count && yarn.scrap %>
-  #       <% if yarn.scrap.to_f == 0.0 && yarn.count == 1 %>
-  #         <li><%= link_to yarn.color, edit_yarn(yarn) %> (<%= yarn.count %> skein)</li>
-  #       <% elsif yarn.scrap.to_f == 0.0 && yarn.count > 1 %>
-  #         <li><%= link_to yarn.color, edit_yarn(yarn) %> (<%= yarn.count %> skeins)</li>
-  #       <% elsif yarn.count == 1 %>
-  #         <li><%= link_to yarn.color, edit_yarn(yarn) %> (<%= yarn.count %> skein, <%= yarn.scrap.to_f %> oz. scrap)</li>
-  #       <% elsif yarn.count > 1 %>
-  #         <li><%= link_to yarn.color, edit_yarn(yarn) %> (<%= yarn.count %> skeins, <%= yarn.scrap.to_f %> oz. scrap)</li>
-  #       <% end %>
-  #     <% elsif yarn.count && yarn.count == 1 %>
-  #       <li><%= link_to yarn.color, edit_yarn(yarn) %> (<%= yarn.count %> skein)</li>
-  #     <% elsif yarn.count && yarn.count > 1 %>
-  #       <li><%= link_to yarn.color, edit_yarn(yarn) %> (<%= yarn.count %> skeins)</li>
-  #     <% elsif yarn.scrap && yarn.scrap.to_f > 0.0 %>
-  #       <li><%= link_to yarn.color, edit_yarn(yarn) %> (<%= yarn.scrap.to_f %> oz. scrap)</li>
-  #     <% end %>
-  #   <% end %>
-  # end
+  def yarn_amount(yarn)
+    if yarn.count && yarn.scrap
+      if yarn.scrap.to_f == 0.0 && yarn.count == 1
+        "(#{yarn.count} skein)"
+      elsif yarn.scrap.to_f == 0.0 && yarn.count > 1
+        "(#{yarn.count} skeins)"
+      elsif yarn.count == 1
+        "(#{yarn.count} skein, #{yarn.scrap.to_f} oz. scrap)"
+      elsif yarn.count > 1
+        "(#{yarn.count} skeins, #{yarn.scrap.to_f} oz. scrap)"
+      end
+    elsif yarn.count && yarn.count == 1
+      "(#{yarn.count} skein)"
+    elsif yarn.count && yarn.count > 1
+      "(#{yarn.count} skeins)"
+    elsif yarn.scrap && yarn.scrap.to_f > 1
+      "(#{yarn.scrap.to_f} oz. scrap)"
+    end
+  end
 
 end
