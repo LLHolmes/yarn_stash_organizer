c492f425ad6b9c93ac8165657e9709d72cbe6486
github username <github email address>
2019-04-11 09:41:49 -0500

    Add tool index on partial to tools & home pages

--- app/helpers/tools_helper.rb
+++ app/helpers/tools_helper.rb
@@ -1,2 +1,11 @@
 module ToolsHelper
+
+  def list_tool
+    if self.project.name == "Stash"
+      "#{self.brand.name} - #{self.color}"
+    else
+      "#{self.brand.name} - #{self.color}  ~  Current Project: #{self.project.name}"
+    end
+  end
+
 end
