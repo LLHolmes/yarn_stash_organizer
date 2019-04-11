64836981c9ff2432afe09b45e2ddd0a9371d70aa
github username <github email address>
2019-04-11 09:42:54 -0500

    Clear unused tool helper

--- app/helpers/tools_helper.rb
+++ app/helpers/tools_helper.rb
@@ -1,11 +1,2 @@
 module ToolsHelper
-
-  def list_tool
-    if self.project.name == "Stash"
-      "#{self.brand.name} - #{self.color}"
-    else
-      "#{self.brand.name} - #{self.color}  ~  Current Project: #{self.project.name}"
-    end
-  end
-
 end
