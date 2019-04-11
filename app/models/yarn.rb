1279d48ed98ce585993a247bc5398484832581c6
github username <github email address>
2019-04-11 15:07:56 -0500

    Add Yarn methods for new/edit function

--- app/models/yarn.rb
+++ app/models/yarn.rb
@@ -2,6 +2,23 @@ class Yarn < ApplicationRecord
   belongs_to :project
   belongs_to :brand
 
+  accepts_nested_attributes_for :project
+  accepts_nested_attributes_for :brand
+
+  def project_attributes=(project_attributes)
+    if !project_attributes[:name].empty?
+      self.project = Project.find_or_create_by(name: project_attributes[:name], user_id: project_attributes[:user_id])
+      self.project.update(project_attributes)
+    end
+  end
+
+  def brand_attributes=(brand_attributes)
+    if !brand_attributes[:name].empty?
+      self.brand = Brand.find_or_create_by(name: brand_attributes[:name])
+      self.brand.update(brand_attributes)
+    end
+  end
+
   def yarn_with_brand
     if self.project.name == "Stash"
       "#{self.brand.name} - #{self.color}"
