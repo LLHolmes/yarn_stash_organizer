2ba962e0ebaffc0dad2a099482bb43d956814063
github username <github email address>
2019-05-13 14:30:31 -0500

    Build brand serializer

--- app/serializers/brand_serializer.rb
+++ app/serializers/brand_serializer.rb
@@ -1,3 +1,5 @@
 class BrandSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :material, :weight, :skein_weight, :skein_length, :hook, :needle
+
+  has_many :yarns
 end
