800634038ea494e79915a57f2e38494ebd4e103a
github username <github email address>
2019-05-15 20:14:46 -0500

    Add created_at to note serializer

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,5 +1,9 @@
 class NoteSerializer < ActiveModel::Serializer
-  attributes :id, :note
-
+  attributes :id, :note, :created
   # belongs_to :project
+
+  def created
+    "#{object.created_at.localtime.strftime("%m/%d/%Y, %l:%M %p")}"
+  end
+
 end
