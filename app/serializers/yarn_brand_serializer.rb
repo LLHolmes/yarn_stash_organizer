65e65c3425030d6577913f2dd04bfcc28819b4f8
github username <github email address>
2019-05-17 10:04:04 -0500

    Add div names to yarn_brand_serializer

--- app/serializers/yarn_brand_serializer.rb
+++ app/serializers/yarn_brand_serializer.rb
@@ -1,3 +1,12 @@
 class YarnBrandSerializer < ActiveModel::Serializer
-  attributes :id, :name, :material, :weight
+  attributes :id, :name, :material, :weight, :nameDiv, :materialDiv
+
+  def nameDiv
+    "#{object.name.downcase.split(" ").join("-")}"
+  end
+
+  def materialDiv
+    "#{object.material.downcase.split(" ").join("-")}"
+  end
+
 end
