5b6e0bdf34ab5e1fae80af11fdcf49755af4d361
LLHolmes <lana.lee.holmes@gmail.com>
2019-07-08 13:22:06 -0500

    Add fallback locale

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module YarnStashOrganizer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
+    config.i18n.fallbacks = [I18n.default_locale]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
