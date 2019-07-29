f5c0cba53b6f87a14b00f4ad1b567b56cfd3ce51
LLHolmes <lana.lee.holmes@gmail.com>
2019-07-29 14:34:38 -0500

    Remove unused belongs_to relationship from NoteSerializer

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,6 +1,5 @@
 class NoteSerializer < ActiveModel::Serializer
   attributes :id, :note, :created
-  # belongs_to :project
 
   def created
     "#{object.created_at.localtime.strftime("%m/%d/%Y, %l:%M %p")}"
