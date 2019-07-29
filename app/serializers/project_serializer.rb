f4d72d4ee54d797d96c27702cfaf8f755009921b
LLHolmes <lana.lee.holmes@gmail.com>
2019-07-29 14:35:13 -0500

    Remove unused relationships from ProjectSerializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,11 +1,9 @@
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :status, :pattern_info, :statusDiv
 
-  # belongs_to :user
   has_many :tools
   has_many :yarns, serializer: ProjectYarnSerializer
   has_many :notes
-  # has_many :brands, serializer: ProjectBrandSerializer
 
   def statusDiv
     "#{object.status.downcase.split(" ").join("-")}"
